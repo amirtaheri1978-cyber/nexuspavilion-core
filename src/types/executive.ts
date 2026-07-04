4f899a73fd61572e218fe5fef7eea0d5bdb36bc4
amirtaheri1978-cyber <a.mirtaheri1978@gmail.com>
2026-07-04 22:31:47 +0000

    Add shared executive quote type

---
+++ src/types/executive.ts
@@ -0,0 +1,13 @@
+export type ExecutiveQuote = {
+rank: number;
+amountNumber: number;
+awardConfidence: number;
+riskLevel: string;
+totalScore: number;
+priceScore: number;
+timelineScore: number;
+riskScore: number;
+performanceScore: number;
+budgetVariance: number;
+lowestBidVariance: number;
+};
